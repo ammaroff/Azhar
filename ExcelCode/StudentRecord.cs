f07824a219558e9d034192b5c6eb27e180a24968
ammar <ammar.osama@readify.net>
2015-06-09 19:59:06 +1000

    تزبيط بعض الملاحظات

--- ExcelCode/StudentRecord.cs
+++ ExcelCode/StudentRecord.cs
@@ -237,7 +237,7 @@ namespace ExcelCode
             //لو كان هناك مادة ثانية وثالثة يضافون بنفس الطريقة
             //ثم تضاف في النهاية كلمة + "لينجح بتقدير"
             bool anyHelp = false;
-            foreach (var row in rows.Where(i => i.HelpDegOnSubj > 0 && i.SubjName != "القرآن الكريم"))
+            foreach (var row in rows.Where(i => i.HelpDegOnSubj > 0 && i.SubjName != "القرآن الكريم" && i.subjectState!="Auto" ))
             {
                 //الجبر الذي يكتب في الملاحظات هو الجبر الذي فوق درجتين .. يمكن معرفة ذلك من خلال 
                 // subjectState = Help
